769d124a4ebf8c3c19636b3c48d1b56838f01fad
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-24 17:06:19 -0500

    Add linalg.h header file

--- inc/csr.h
+++ inc/csr.h
@@ -1,12 +1,20 @@
 #ifndef _CSR_H_
 #define _CSR_H_
 
-struct csr {
+#include "linalg.h"
+
+//------------------------------------------------------------------------------
+// CSRMatrix structure
+struct genmap_csr {
   int *ia;
   int *ja;
   int *va;
 };
+typedef struct genmap_csr CSRMatrix;
 
-typedef struct csr CSRMatrix;
+//------------------------------------------------------------------------------
+// CSRMatrix vector multiplication
+void csr_matrix_vector_multiply(Vector *y, CSRMatrix *A, Vector *x);
+//------------------------------------------------------------------------------
 
 #endif
---
+++ inc/linalg.h
@@ -0,0 +1,10 @@
+#ifndef _LINALG_H_
+#define _LINALG_H_
+
+struct genmap_vector {
+  int size;
+  double *vv;
+};
+typedef struct genmap_vector Vector;
+
+#endif
