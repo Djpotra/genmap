47710e921f70f7ce37d3dae875e3d7d3bc6c238d
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-25 13:26:33 -0500

    Add a test for random_vector

--- tests/vector.c
+++ tests/vector.c
@@ -32,10 +32,20 @@ int test_3() {
   return (y.size == 6 && vectors_equal(&y, &answer, 1e-12));
 }
 //------------------------------------------------------------------------------
+int test_4() {
+  Vector y;
+
+  random_vector(&y, 6);
+
+  // TODO: add a test to check if inf_norm(y) <= 1.
+  return (y.size == 6);
+}
+//------------------------------------------------------------------------------
 int main() {
   run_test(&test_1,"vector_1");
   run_test(&test_2,"vector_2");
   run_test(&test_3,"vector_3");
+  run_test(&test_4,"vector_4");
 
   return 0;
 }
