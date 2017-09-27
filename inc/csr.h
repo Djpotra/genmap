bdba6073a6251888f430cc27e3cfa5e0c067bcc3
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-27 01:16:30 -0500

    Add a test -- failing ...

--- inc/csr.h
+++ inc/csr.h
@@ -20,7 +20,8 @@ void csr_matrix_vector_multiply(Vector *y, CSRMatrix *A, Vector *x);
 //void csr_init(CSRMatrix *A, ....
 
 void lanczos(Vector *alpha, Vector *beta, CSRMatrix *A, Vector *init);
-void lanczos_ggavl(Vector *alpha, Vector *beta, CSRMatrix *A);
+
+void lanczos2(Vector *alpha, Vector *beta, CSRMatrix *A, Vector *init);
 //------------------------------------------------------------------------------
 
 #endif
--- tests/lanczos_test.c
+++ tests/lanczos_test.c
@@ -31,6 +31,33 @@ int test_1() {
   return 1;
 }
 //------------------------------------------------------------------------------
+int test_1_2() {
+  int         n = 5;
+  double va[13] = {2, 1,          \
+                   1, 2, 1,       \
+                      1, 2, 1,    \
+                         1, 2, 1, \
+                            1, 2};
+  int ja[13]   = {0, 1,          \
+                  0, 1, 2,       \
+                     1, 2, 3,    \
+                        2, 3, 4, \
+                           3, 4};
+  int ia[6]    = {0, 2, 5, 8, 11, 13};
+  CSRMatrix A = { .nrows = n, .ia = ia, .ja = ja, .va = va };
+
+  Vector alpha, beta, init;
+  zeros_vector(&alpha, n); zeros_vector(&beta, n - 1);
+  ones_vector(&init, n);
+
+  lanczos2(&alpha, &beta, &A, &init);
+
+  printf("\nalpha: "); print_vector(&alpha);
+  printf("\nbeta: "); print_vector(&beta);
+
+  return 1;
+}
+//------------------------------------------------------------------------------
 int test_2() {
   int         n = 5;
   double va[13] = {2, 3,          \
@@ -61,6 +88,7 @@ int test_2() {
 int main() {
   run_test(&test_1, "lanczos1");
   run_test(&test_2, "lanczos2");
+  run_test(&test_1_2, "lanczos2_1");
 
   return 0;
 }
