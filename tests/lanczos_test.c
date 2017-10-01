ead1fb3cca1fbd8b5e8d45587d1007a908ee62d2
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-10-01 18:21:35 -0500

    Add more tests;
    Test 3 is weired

--- tests/lanczos_test.c
+++ tests/lanczos_test.c
@@ -112,11 +112,87 @@ int test_2_2() {
   return 1;
 }
 //------------------------------------------------------------------------------
+int test_3() {
+  int         n = 4;
+  double va[12] = { 2, -1, -1      \
+                   -1,  2, -1,     \
+                   -1, -1,  3, -1, \
+                           -1,  1};
+  int ja[12]   = {0, 1, 2,      \
+                  0, 1, 2,      \
+                  0, 1, 2, 3,   \
+                        2, 3,};
+  int ia[6]    = {0, 3, 6, 10, 12};
+  CSRMatrix A = { .nrows = n, .ia = ia, .ja = ja, .va = va };
+
+  Vector alpha, beta, init;
+  zeros_vector(&alpha, n); zeros_vector(&beta, n - 1);
+  ones_vector(&init, n);
+
+  lanczos(&alpha, &beta, &A, &init);
+
+  printf("\nalpha: "); print_vector(&alpha);
+  printf("\nbeta: "); print_vector(&beta);
+
+  return 1;
+}
+//------------------------------------------------------------------------------
+int test_3_2() {
+  int         n = 4;
+  double va[12] = { 2, -1, -1      \
+                   -1,  2, -1,     \
+                   -1, -1,  3, -1, \
+                           -1,  1};
+  int ja[12]   = {0, 1, 2,      \
+                  0, 1, 2,      \
+                  0, 1, 2, 3,   \
+                        2, 3,};
+  int ia[6]    = {0, 3, 6, 10, 12};
+  CSRMatrix A = { .nrows = n, .ia = ia, .ja = ja, .va = va };
+
+  Vector alpha, beta, init;
+  zeros_vector(&alpha, n); zeros_vector(&beta, n - 1);
+  ones_vector(&init, n);
+
+  lanczos2(&alpha, &beta, &A, &init);
+
+  printf("\nalpha: "); print_vector(&alpha);
+  printf("\nbeta: "); print_vector(&beta);
+
+  return 1;
+}
+//------------------------------------------------------------------------------
+int test_4() {
+  int         n = 3;
+  double va[9] = { 1, 2, 3,     \
+                   2, 4, 5,     \
+                   3, 5, 6 };
+  int ja[9]   = {0, 1, 2,      \
+                  0, 1, 2,      \
+                  0, 1, 2 };
+  int ia[9]    = {0, 3, 6, 9};
+  CSRMatrix A = { .nrows = n, .ia = ia, .ja = ja, .va = va };
+
+  Vector alpha, beta, init;
+  zeros_vector(&alpha, n); zeros_vector(&beta, n - 1);
+  ones_vector(&init, n);
+
+  lanczos(&alpha, &beta, &A, &init);
+
+  printf("\nalpha: "); print_vector(&alpha);
+  printf("\nbeta: "); print_vector(&beta);
+
+  return 1;
+}
+//------------------------------------------------------------------------------
 int main() {
   run_test(&test_1, "lnczs1");
   run_test(&test_1_2, "lnczs1_2");
   run_test(&test_2, "lnczs2");
   run_test(&test_2_2, "lnczs2_2");
+  run_test(&test_3, "lnczs3");
+  run_test(&test_3_2, "lnczs3_2");
+  run_test(&test_4, "lnczs4");
 
   return 0;
 }
