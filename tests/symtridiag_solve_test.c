5969c4dfafabbb33acbb1f49c5d9f848fb2ef8ee
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-12-02 22:17:57 -0600

    Add a new test

--- tests/symtridiag_solve_test.c
+++ tests/symtridiag_solve_test.c
@@ -10,23 +10,46 @@ int test_1()
 
   ones_vector (&alpha, n   );
   zeros_vector(&beta, n - 1);
-//  zeros_vector(&x, n       );
   ones_vector (&b, n       );
 
-  symtridiag_solve(&x, &b, &alpha, &beta); 
+  symtridiag_solve(&x, &b, &alpha, &beta);
 
   for (int32 i = 0; i < n; i++) {
     printf("%lf, ", x.vv[i]);
   }
   printf("\n");
 
-  return 0;
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+int test_2()
+{
+  int32 n = 4;
+  Vector alpha, beta, b, x;
+
+  ones_vector(&alpha, n   ); scale_vector(&alpha, &alpha, 2.0);
+  ones_vector(&beta, n - 1); scale_vector(&beta, &beta, -1.0 );
+  ones_vector(&b, n       );
+
+  symtridiag_solve(&x, &b, &alpha, &beta);
+
+  for (int32 i = 0; i < n; i++) {
+    printf("%lf, ", x.vv[i]);
+  }
+  printf("\n");
+
+  Vector correct; create_vector(&correct, n);
+  correct.vv[0]=2.0, correct.vv[1]=3.0, correct.vv[2]=3.0, correct.vv[3]=2.0;
+
+  return vectors_equal(&correct, &x, 10e-8);
 }
 
 //------------------------------------------------------------------------------
 int32 main(int32 argc, char** argv)
 {
-  run_test(&test_1, "symtridiag_solve");
+  run_test(&test_1, "symtridiag_solve_1");
+  run_test(&test_2, "symtridiag_solve_2");
 
   return 0;
 }
