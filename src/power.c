dc12813fa9329914bd8a6cff9847b741e3eb02b8
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-12-02 22:56:49 -0600

    Add inverse power iteration

--- src/power.c
+++ src/power.c
@@ -29,7 +29,7 @@ void power(Vector *eVector, Vector *alpha, Vector *beta, Vector *init,
       }
       y.vv[n - 1] = beta->vv[n - 2]*x.vv[n - 2] + alpha->vv[n - 1]*x.vv[n - 1];
 
-      // calculate infnty norm(y)
+      // calculate 2-norm(y)
       scale_vector(&y, &y, 1.0/norm_vector(&y, 2));
 
       copy_vector (&x, &y);
@@ -57,15 +57,10 @@ void invpower(Vector *eVector, Vector *alpha, Vector *beta, Vector *init,
     return;
   } else {
     for (int32 j = 0; j < iter; j++) {
-      // y = Ax
-      y.vv[0] = alpha->vv[0]*x.vv[0] + beta->vv[0]*x.vv[1];
-      for (int32 i = 1; i < n - 1; i++) {
-        y.vv[i] = beta->vv[i - 1]*x.vv[i - 1] + alpha->vv[i]*x.vv[i] +
-                                                    beta->vv[i]*x.vv[i + 1];
-      }
-      y.vv[n - 1] = beta->vv[n - 2]*x.vv[n - 2] + alpha->vv[n - 1]*x.vv[n - 1];
+      // Ay = x
+      symtridiag_solve(&y, &x, &alpha, &beta);
 
-      // calculate infnty norm(y)
+      // calculate 2-norm(y)
       scale_vector(&y, &y, 1.0/norm_vector(&y, 2));
 
       copy_vector (&x, &y);
