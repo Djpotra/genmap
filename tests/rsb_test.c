e851dbfc88b85c6ac20371bbeb6b61df09e0131c
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-12-06 19:32:14 -0600

    Binary files is in 32bit now

--- tests/rsb_test.c
+++ tests/rsb_test.c
@@ -1,4 +1,5 @@
 #include "mpiwrapper.h"
+#include "io.h"
 
 //------------------------------------------------------------------------------
 int32 main(int32 argc, char** argv)
@@ -6,6 +7,27 @@ int32 main(int32 argc, char** argv)
   struct comm global;
 
   init_genmap(&global, argc, argv);
+
+  char *name = "nbrhd/nbrhd.map.bin";
+  int32 *header, *glo_num, *elem_id;
+
+  readmap(&global, &header, &glo_num, &elem_id, name);
+
+#ifdef DEBUG
+  for (int32 i = 0; i < HEADER_SIZE; i++)
+  {
+    printf("%d ", header[i]);
+  }
+  printf("\n");
+
+  int32 i = 0;
+  while (i < header[NC]*header[MYCHUNK])
+  {
+    printf("%d ", glo_num[i]);
+    i++;
+    if (i%header[NC] == 0) printf("\n");
+  }
+#endif
 
   finalize_genmap(&global);
    
