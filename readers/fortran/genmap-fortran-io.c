af7b7c02a28c63e732c87c26b692e30933cdf253
Thilina Rathnayake <thilinarmtb@gmail.com>
2018-06-21 19:05:46 -0500

    Bug fix and polishing Fortran reader

--- readers/fortran/genmap-fortran-io.c
+++ readers/fortran/genmap-fortran-io.c
@@ -9,28 +9,22 @@ int GenmapRead_fortran(GenmapHandle h, void *data) {
 
   GenmapInt nel = h->header->nel;
   GenmapInt nv  = h->header->nv;
-  GenmapInt lelt = nel / h->Np(h->global);
-
-#ifdef MPI
-  if(h->Id(h->global) == h->Np(h->global) - 1)
-    lelt = nel - h->Id(h->global) * lelt;
-#endif
-  h->header->lelt = lelt;
 
   GenmapInt out[2][1], buf[2][1];
-  comm_scan(out, &(h->global->gsComm), gs_int, gs_add, &lelt, 1, buf);
+  comm_scan(out, &(h->global->gsComm), gs_int, gs_add, &nel, 1, buf);
   h->header->start = out[0][0];
 
-  array_reserve(struct GenmapElement_private, &(h->elementArray), lelt);
-  h->elementArray.n = lelt;
+  array_reserve(struct GenmapElement_private, &(h->elementArray), nel);
+  h->elementArray.n = nel;
   GenmapElements elements = GenmapGetElements(h);
 
   GenmapInt count = 0;
   GenmapInt start = h->header->start;
-  for(GenmapInt i = 0; i < lelt; i++) {
+  for(GenmapInt i = 0; i < nel; i++) {
     elements[i].globalId = start + i;
     for(GenmapInt j = 0; j < nv; j++) {
       elements[i].vertices[j] = vertices[count];
+      count++;
     }
   }
 
--- readers/fortran/genmap-fortran.c
+++ readers/fortran/genmap-fortran.c
@@ -1,9 +1,6 @@
 #include <genmap-readers.h>
 
 #include <stdio.h>
-//
-// GenmapHandle: Create, Destroy
-//
 int GenmapCreateHandle_fortran(GenmapHandle h) {
   h->Read = GenmapRead_fortran;
 
